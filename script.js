4cda2adcffb5ee1939ec4850ac4a80d2e68b8992
mugiisha <mugishabankz3@gmail.com>
2022-03-03 18:47:07 +0200

    added some js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const toggleBtn = document.getElementsByClassName('toggle-btn')[0]
-const navBtn = document.getElementsByClassName('nav-btn')[0]
+const toggleBtn = document.getElementsByClassName('toggle-btn')[0];
+const navBtn = document.getElementsByClassName('nav-btn')[0];
 
-toggleBtn.addEventListener('click', () =>{
-    navBtn.classList.toggle('active')
+toggleBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    navBtn.classList.toggle('active');
 })
 
 const about = document.getElementById('about');
