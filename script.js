bb0449f17a7d48b3a8260a657d100323913cee2e
mugiisha <mugishabankz3@gmail.com>
2022-03-03 18:45:01 +0200

    added som js

--- script.js
+++ script.js
@@ -27,6 +27,20 @@ newsLetter.addEventListener('click', () =>{
     prompt('enter your email and subscribe to our newsletter')
 })
 
-const blogs = document.getElementsByClassName('blog');
+const blogs = document.getElementsByClassName('blog-img');
 console.log(blogs.item(0).innerHTML)
 
+// firebase image
+
+// Create a root reference
+var storageRef = firebase.storage().ref();
+
+// Create a reference to 'mountains.jpg'
+var mountainsRef = storageRef.child('blog.jpg');
+
+// Create a reference to 'images/mountains.jpg'
+var mountainImagesRef = storageRef.child('images/blog-1.jpg');
+
+// While the file names are the same, the references point to different files
+mountainsRef.name === mountainImagesRef.name;           // true
+mountainsRef.fullPath === mountainImagesRef.fullPath;   // false 
